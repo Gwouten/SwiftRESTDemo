1f5f75f4373bed9ed3c0521f5175ff1879cefed4
Wouter Deloof <wouter.deloof@gmail.com>
2020-01-22 10:02:48 +0100

    initial commit

---
+++ SwiftRESTDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftRESTDemo
+//
+//  Created by mobapp15 on 22/01/2020.
+//  Copyright © 2020 mobapp15. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
